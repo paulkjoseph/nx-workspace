46993453bc22a0681db19427b05b7cb23c26b1f3
Paul Kibe <wacujuguna@gmail.com>
2018-06-12 11:17:59 -0300

    Changed the ChangeDetectionStrategy to OnPush

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
 
-  login(authenticate:any) {
+  login(authenticate: any) {
     console.log(authenticate);
   }
 }
